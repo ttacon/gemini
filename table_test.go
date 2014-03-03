b0470c5f0e44ee7e1fdbbb9277aeedc72408fe60
ttacon <ttacon@gmail.com>
2014-03-03 10:01:05 -0500

    add some more tests

--- table_test.go
+++ table_test.go
@@ -5,6 +5,65 @@ import (
 	"testing"
 )
 
+const (
+	STRING_CONST = ""
+	INT_CONST    = 0
+)
+
+type dbFieldTest struct {
+	in  interface{}
+	out []dbField
+}
+
+type Struct2 struct {
+	Id   int    `dbInfo:"primaryKey,autoIncr,notNull"`
+	Name string `db:"DbName"`
+}
+
+var dbFieldTests = []*dbFieldTest{
+	&dbFieldTest{
+		in: Struct2{},
+		out: []dbField{
+			dbField{
+				fieldName:    "Id",
+				columnName:   "Id",
+				joinsTo:      nil,
+				goType:       reflect.TypeOf(INT_CONST),
+				isPrimaryKey: true,
+				isAutoIncr:   true,
+				isNotNull:    true,
+			},
+			dbField{
+				fieldName:    "Name",
+				columnName:   "DbName",
+				joinsTo:      nil,
+				goType:       reflect.TypeOf(STRING_CONST),
+				isPrimaryKey: false,
+				isAutoIncr:   false,
+				isNotNull:    false,
+			},
+		},
+	},
+}
+
+func Test_getFieldsFor(t *testing.T) {
+	for _, test := range dbFieldTests {
+		fields := getFieldsFor(test.in)
+		if len(fields) != len(test.out) {
+			t.Errorf("expected %d fields, got %d",
+				len(test.out),
+				len(fields))
+		}
+		for i := 0; i < len(fields); i++ {
+			if fields[i] != test.out[i] {
+				t.Errorf("fields differ, expected: %#v, got: %#v",
+					test.out[i],
+					fields[i])
+			}
+		}
+	}
+}
+
 type Struct1 struct {
 	field1         interface{} `db:"field1" expected:"field1"`
 	field3         interface{} `expected:"field3"`
@@ -76,6 +135,89 @@ func Test_parseJoinInfo(t *testing.T) {
 				"incorrectly parsed join info, got: %v, expected: %v",
 				j,
 				test.out)
+		}
+	}
+}
+
+type dbTest struct {
+	in string
+	isPrimaryKey,
+	isAutoIncr,
+	isNotNull bool
+}
+
+var dbTests = []*dbTest{
+	&dbTest{
+		"",
+		false,
+		false,
+		false,
+	},
+	&dbTest{
+		"primaryKey",
+		true,
+		false,
+		false,
+	},
+	&dbTest{
+		"primaryKey,autoIncr",
+		true,
+		true,
+		false,
+	},
+	&dbTest{
+		"primaryKey,notNull",
+		true,
+		false,
+		true,
+	},
+	&dbTest{
+		"primaryKey,autoIncr,notNull",
+		true,
+		true,
+		true,
+	},
+	&dbTest{
+		"autoIncr,notNull",
+		false,
+		true,
+		true,
+	},
+	&dbTest{
+		"autoIncr",
+		false,
+		true,
+		false,
+	},
+	&dbTest{
+		"notNull",
+		false,
+		false,
+		true,
+	},
+}
+
+func Test_dbInfo(t *testing.T) {
+	for _, test := range dbTests {
+		if tagIsPrimaryKey(test.in) != test.isPrimaryKey {
+			t.Error(
+				"tag was primary key: %v, was expected to be: %v",
+				tagIsPrimaryKey(test.in),
+				test.isPrimaryKey)
+		}
+
+		if tagIsAutoIncr(test.in) != test.isAutoIncr {
+			t.Error(
+				"tag was auto incr: %v, was expected to be: %v",
+				tagIsAutoIncr(test.in),
+				test.isAutoIncr)
+		}
+
+		if tagIsNotNull(test.in) != test.isNotNull {
+			t.Error(
+				"tag was not null: %v, was expected to be: %v",
+				tagIsNotNull(test.in),
+				test.isNotNull)
 		}
 	}
 }
